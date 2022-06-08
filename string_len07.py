3321e6db8267ca379c50e5873191f7ae8f9b4410
Ikrom <ikromyusupov09@gmail.com>
2022-06-08 18:39:27 +0500

    Given three strings, s1, s2 and s3. return their odd lengths

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,21 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    l1 = len(s1) % 2
+    l2 = len(s2) % 2
+    l3 = len(s3) % 2
+    length = l1 + l2 + l3
+    if l1 + l2 + l3 == 0:
+        return '[]'
+    if l1 and l2 and l3:
+        ans = f"[{s1}, {s2}, {s3}]"
+    elif l1 and l2:
+        ans = f"[{s1}, {s2}]"
+    elif l1 and l3:
+        ans = f"[{s1}, {s3}]"
+    elif l2 and l3:
+        ans = f"[{s2}, {s3}]"
+    else:
+        ans = f"[{s1 * l1}{s2 * l2}{s3 * l3}]"
+
+    return ans
